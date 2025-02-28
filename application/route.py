8a7b6c37e65844f5967cdc900cb787475795580e
isiborella <isiborella98@gmail.com>
2025-02-28 08:45:25 +0100

    fourth commit

--- application/route.py
+++ application/route.py
@@ -1,7 +1,7 @@
 from flask import Flask,redirect,url_for,render_template,request
-import config
+
 app=Flask(__name__)
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
