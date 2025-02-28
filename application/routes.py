35a3b39769710233a3ec68f99373e5204ebfe4d1
isiborella <isiborella98@gmail.com>
2025-02-28 08:43:49 +0100

    third commit

--- application/routes.py
+++ application/routes.py
@@ -3,10 +3,7 @@ import config
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('index.html')
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host=config.HOST,port=config.PORT,debug=True)
+    app.run(debug=True)
