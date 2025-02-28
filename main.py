49c3c3f35581d75e63ba7262ba592350af33ec99
isiborella <isiborella98@gmail.com>
2025-02-28 08:51:24 +0100

    commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask,redirect,url_for,render_template,request
 import config
-from application import app
-
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
