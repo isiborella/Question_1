e4003c296125cf4373a56da2746dcef8aa9ec4d6
isiborella <isiborella98@gmail.com>
2025-02-28 08:39:28 +0100

    second commit

---
+++ application/routes.py
@@ -0,0 +1,12 @@
+from flask import Flask,redirect,url_for,render_template,request
+import config
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('index.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host=config.HOST,port=config.PORT,debug=True)
---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask,redirect,url_for,render_template,request
+import config
+from application import app
+
+
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(host=config.HOST,port=config.PORT,debug=True)
